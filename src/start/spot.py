b347ca107dc0f6c145f384597df1064390e5c6ad
sazecis <csaba.szegedi@sazecis.com>
2023-01-25 22:11:59 +0100

    cleanup

--- src/start/spot.py
+++ src/start/spot.py
@@ -15,23 +15,10 @@ def get_spot_prices(instance_type):
             'Linux/UNIX',
         ],
     )
-    #minPrice = decimal.Decimal(1000)
     historyPrices = {}
     histories = data['SpotPriceHistory']
     print(histories)
-    #bestSpot = None
     for history in histories:
         historyPrices[history['AvailabilityZone']] = decimal.Decimal(
             history['SpotPrice']) + decimal.Decimal(0.005)
-        #historyPrices.append(decimal.Decimal(history['SpotPrice']) + decimal.Decimal(0.005))
-        '''if historyPrice < minPrice:
-            minPrice = historyPrice
-            history['SpotPrice'] = historyPrice + decimal.Decimal(0.005)
-            bestSpot = history
-            print('spot price of ' + instance_type + ' in region ' +
-                  common_config.REGION + ': ' + str(minPrice))'''
-    '''sorted_histories = {}
-    for price in sorted(historyPrices):
-        SpotPrice
-        AvailabilityZone'''
     return dict(sorted(historyPrices.items(), key=lambda x: x[1]))
